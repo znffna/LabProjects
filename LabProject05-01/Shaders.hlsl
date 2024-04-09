2bcbbdbf09be7bcfcd5e854f489b5d3e758958d6
znffna <znffna@gmail.com>
2024-04-09 23:39:34 +0900

    따라하기(06-01).pdf - 픽셀 쉐이더(8)
    Rectangle 2개

--- LabProject05-01/Shaders.hlsl
+++ LabProject05-01/Shaders.hlsl
@@ -36,18 +36,24 @@ inline bool IsEqual(float fA, float fB, float fEpsilon)
 fEpsilon));
 }
 
+float Rectangle(float2 f2NDC, float fLeft, float fRight, float fTop, float fBottom)
+{
+    float2 f2Shape = float2(step(fLeft, f2NDC.x), step(f2NDC.x, fRight));
+    f2Shape *= float2(step(fTop, f2NDC.y), step(f2NDC.y, fBottom));
+    return (f2Shape.x * f2Shape.y);
+}
+
 //«»ºø ºŒ¿Ã¥ı∏¶ ¡§¿««—¥Ÿ.
 float4 PSMain(float4 input : SV_POSITION) : SV_TARGET
 {
     float4 cColor = float4(0.0f, 0.0f, 0.0f, 1.0f);
     
-    if ((int) input.x == (int) HALF_WIDTH)
-        cColor.g = 1.0f;
-    if ((int) input.y == (int) HALF_HEIGHT)
-        cColor.r = 1.0f;
-    float fDistance = distance((int2) input.xy, float2(HALF_WIDTH, HALF_HEIGHT));
-    if (IsEqual(fDistance, 100.0f, 0.5f))
-        cColor.b = 1.0f;
+    float2 f2NDC = float2(input.x / FRAME_BUFFER_WIDTH, input.y / FRAME_BUFFER_HEIGHT) -0.5f; // (0, 1) : (-0.5, 0.5)
+    f2NDC.x *= (FRAME_BUFFER_WIDTH / FRAME_BUFFER_HEIGHT);
+    float2 f2Horizontal = float2(0.1f, +0.3f); //(Left, Right)
+    float2 f2Vertical = float2(-0.3f, +0.3f); //(Top, Bottom)
+    cColor.b = Rectangle(f2NDC, +0.1f, +0.3f, -0.2f, +0.4f);
+    cColor.b += Rectangle(f2NDC, -0.3f, -0.1f, -0.4f, -0.1f);
     
     return (cColor);
 }
