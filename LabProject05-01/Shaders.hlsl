d8963731442728262ddd5253a68ce2c437374f2b
znffna <znffna@gmail.com>
2024-04-09 23:42:12 +0900

    따라하기(06-01).pdf - 픽셀 쉐이더(10)
    원을 정점으로만 표현.
    정점이 부드러운 브러쉬로 그려진 효과를 가진다.

--- LabProject05-01/Shaders.hlsl
+++ LabProject05-01/Shaders.hlsl
@@ -48,10 +48,15 @@ float4 PSMain(float4 input : SV_POSITION) : SV_TARGET
 {
     float4 cColor = float4(0.0f, 0.0f, 0.0f, 1.0f);
     
-    float2 f2NDC = float2(input.x - FRAME_BUFFER_WIDTH * 0.5f, input.y -FRAME_BUFFER_HEIGHT * 0.5f);
-    f2NDC *= 20.0f;
-    float fLength = length(f2NDC);
-    cColor.rgb = cos(fLength);
+    float2 f2NDC = float2(input.x / FRAME_BUFFER_WIDTH, input.y / FRAME_BUFFER_HEIGHT) -
+0.5f; // (0, 1) : (-0.5, 0.5)
+    f2NDC.x *= (FRAME_BUFFER_WIDTH / FRAME_BUFFER_HEIGHT);
+    float fRadius = 0.3f;
+    float fRadian = radians(360.0f / 30.0f);
+    for (float f = 0; f < 30.0f; f += 1.0f) {
+        float fAngle = fRadian * f;
+        cColor.rgb += (0.0025f / length(f2NDC + float2(fRadius * cos(fAngle), fRadius * sin(fAngle))));
+    }
     
     return (cColor);
 }
