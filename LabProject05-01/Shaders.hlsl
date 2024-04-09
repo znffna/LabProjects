f9e892871b2c761fb21fcbb723cc5d8ae90316a1
znffna <znffna@gmail.com>
2024-04-09 18:42:32 +0900

    따라하기(06-01).pdf - 픽셀 쉐이더(2)
    Red - x, Green - y

--- LabProject05-01/Shaders.hlsl
+++ LabProject05-01/Shaders.hlsl
@@ -25,6 +25,7 @@ float4 PSMain(float4 input : SV_POSITION) : SV_TARGET
     float4 cColor = float4(0.0f, 0.0f, 0.0f, 1.0f);
    
     cColor.r = input.x / FRAME_BUFFER_WIDTH;
+    cColor.g = input.y / FRAME_BUFFER_HEIGHT;
     
     return (cColor);
 }
