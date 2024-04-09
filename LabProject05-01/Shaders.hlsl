85fc2b333e427270968e199b485860ab561f332c
znffna <znffna@gmail.com>
2024-04-09 23:45:11 +0900

    따라하기(06-01).pdf - 픽셀 쉐이더(11)
    팔각형과 사각형

--- LabProject05-01/Shaders.hlsl
+++ LabProject05-01/Shaders.hlsl
@@ -19,44 +19,22 @@ float4 VSMain(uint nVertexID : SV_VertexID) : SV_POSITION
 #define FRAME_BUFFER_WIDTH      800.0f
 #define FRAME_BUFFER_HEIGHT      600.0f
 
-#define HALF_WIDTH (FRAME_BUFFER_WIDTH * 0.5f)
-#define HALF_HEIGHT (FRAME_BUFFER_HEIGHT * 0.5f)
-#define EPSILON 1.0e-5f
-inline bool IsZero(float fValue)
-{
-    return ((abs(fValue) <= EPSILON));
-}
-inline bool IsZero(float fValue, float fEpsilon)
-{
-    return ((abs(fValue) <= fEpsilon));
-}
-inline bool IsEqual(float fA, float fB, float fEpsilon)
-{
-    return ((abs(fA - fB) <=
-fEpsilon));
-}
-
-float Rectangle(float2 f2NDC, float fLeft, float fRight, float fTop, float fBottom)
-{
-    float2 f2Shape = float2(step(fLeft, f2NDC.x), step(f2NDC.x, fRight));
-    f2Shape *= float2(step(fTop, f2NDC.y), step(f2NDC.y, fBottom));
-    return (f2Shape.x * f2Shape.y);
-}
-
 //ÇÈ¼¿ ¼ÎÀÌ´õ¸¦ Á¤ÀÇÇÑ´Ù.
+float RegularPolygon(float2 f2NDC, float fSides, float fRadius)
+{
+    float fAngle = atan(f2NDC.y / f2NDC.x);
+    float fSlices = (2.0f * 3.14159f) / fSides;
+    float fShape = step(cos(floor((fAngle / fSlices) + 0.5f) * fSlices - fAngle) *length(f2NDC), fRadius);
+    return (fShape);
+}
+
 float4 PSMain(float4 input : SV_POSITION) : SV_TARGET
 {
     float4 cColor = float4(0.0f, 0.0f, 0.0f, 1.0f);
-    
-    float2 f2NDC = float2(input.x / FRAME_BUFFER_WIDTH, input.y / FRAME_BUFFER_HEIGHT) -
-0.5f; // (0, 1) : (-0.5, 0.5)
+    float2 f2NDC = float2(input.x / FRAME_BUFFER_WIDTH, input.y / FRAME_BUFFER_HEIGHT) -0.5f; // (0, 1) : (-0.5, 0.5)
     f2NDC.x *= (FRAME_BUFFER_WIDTH / FRAME_BUFFER_HEIGHT);
-    float fRadius = 0.3f;
-    float fRadian = radians(360.0f / 30.0f);
-    for (float f = 0; f < 30.0f; f += 1.0f) {
-        float fAngle = fRadian * f;
-        cColor.rgb += (0.0025f / length(f2NDC + float2(fRadius * cos(fAngle), fRadius * sin(fAngle))));
-    }
-    
+    cColor.b = RegularPolygon(f2NDC - float2(-0.3f, -0.1f), 8.0f, 0.2f); //4, 6, 8, ...
+    cColor.r = RegularPolygon(f2NDC - float2(+0.3f, +0.2f), 4.0f, 0.15f);
     return (cColor);
 }
+
