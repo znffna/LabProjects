96b696954fbbe700ad48741e42271bd16decc99c
znffna <znffna@gmail.com>
2024-04-09 23:46:47 +0900

    따라하기(06-01).pdf - 픽셀 쉐이더(13)
    최면파 모양 ◎

--- LabProject05-01/Shaders.hlsl
+++ LabProject05-01/Shaders.hlsl
@@ -31,9 +31,10 @@ float RegularPolygon(float2 f2NDC, float fSides, float fRadius)
 float4 PSMain(float4 input : SV_POSITION) : SV_TARGET
 {
     float4 cColor = float4(0.0f, 0.0f, 0.0f, 1.0f);
-    float x = abs(frac((input.x * 10.0f) / FRAME_BUFFER_HEIGHT) - 0.5f);
-    float y = abs(frac((input.y * 10.0f) / FRAME_BUFFER_HEIGHT) - 0.5f);
-    cColor.r = ((x <= 0.0125f) || (y <= 0.0125f)) ? 1.0f : 0.0f;
+    float2 f2NDC = input.xy / float2(FRAME_BUFFER_WIDTH, FRAME_BUFFER_HEIGHT) - 0.5f;
+    f2NDC.x *= (FRAME_BUFFER_WIDTH / FRAME_BUFFER_HEIGHT);
+    f2NDC.xy *= 10.0f;
+    cColor.b = smoothstep(0.125f, 0.875f, abs(cos(length(f2NDC) * 3.14159f)));
     return (cColor);
 }
 
