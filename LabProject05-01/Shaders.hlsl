3610bd7a10ecd91de8c0408e497cc38b0359da25
znffna <znffna@gmail.com>
2024-04-09 23:40:50 +0900

    따라하기(06-01).pdf - 픽셀 쉐이더(9)
    * 모양

--- LabProject05-01/Shaders.hlsl
+++ LabProject05-01/Shaders.hlsl
@@ -48,12 +48,10 @@ float4 PSMain(float4 input : SV_POSITION) : SV_TARGET
 {
     float4 cColor = float4(0.0f, 0.0f, 0.0f, 1.0f);
     
-    float2 f2NDC = float2(input.x / FRAME_BUFFER_WIDTH, input.y / FRAME_BUFFER_HEIGHT) -0.5f; // (0, 1) : (-0.5, 0.5)
-    f2NDC.x *= (FRAME_BUFFER_WIDTH / FRAME_BUFFER_HEIGHT);
-    float2 f2Horizontal = float2(0.1f, +0.3f); //(Left, Right)
-    float2 f2Vertical = float2(-0.3f, +0.3f); //(Top, Bottom)
-    cColor.b = Rectangle(f2NDC, +0.1f, +0.3f, -0.2f, +0.4f);
-    cColor.b += Rectangle(f2NDC, -0.3f, -0.1f, -0.4f, -0.1f);
+    float2 f2NDC = float2(input.x - FRAME_BUFFER_WIDTH * 0.5f, input.y -FRAME_BUFFER_HEIGHT * 0.5f);
+    f2NDC *= 20.0f;
+    float fLength = length(f2NDC);
+    cColor.rgb = cos(fLength);
     
     return (cColor);
 }
