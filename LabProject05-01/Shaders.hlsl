d6ec6ee73d94c7f43ef47b1c9a37d84c25e5ce28
znffna <znffna@gmail.com>
2024-04-09 18:35:06 +0900

    따라하기(06-01).pdf - 픽셀 쉐이더(1)
    BLACK - RED Gradation

---
+++ LabProject05-01/Shaders.hlsl
@@ -0,0 +1,30 @@
+float4 VSMain(uint nVertexID : SV_VertexID) : SV_POSITION
+{
+    float4 output = (float4)0;
+    
+//프리미티브(삼각형)를 구성하는 정점의 인덱스(SV_VertexID)에 따라 정점을 반환한다. 
+//정점의 위치 좌표는 변환이 된 좌표(SV_POSITION)이다. 즉, 투영좌표계의 좌표이다. 
+    if (nVertexID == 0) output = float4(-1.0f, +1.0f, 0.0f, 1.0f);
+    else if (nVertexID == 1) output = float4(+1.0f, +1.0f, 0.0f, 1.0f);
+    else if (nVertexID == 2) output = float4(+1.0f, -1.0f, 0.0f, 1.0f);
+    
+    else if (nVertexID == 3) output = float4(-1.0f, +1.0f, 0.0f, 1.0f);
+    else if (nVertexID == 4) output = float4(+1.0f, -1.0f, 0.0f, 1.0f);
+    else if (nVertexID == 5) output = float4(-1.0f, -1.0f, 0.0f, 1.0f);
+    
+    return (output);
+
+}
+
+#define FRAME_BUFFER_WIDTH      640.0f
+#define FRAME_BUFFER_HEIGHT      480.0f
+
+//픽셀 셰이더를 정의한다.
+float4 PSMain(float4 input : SV_POSITION) : SV_TARGET
+{
+    float4 cColor = float4(0.0f, 0.0f, 0.0f, 1.0f);
+   
+    cColor.r = input.x / FRAME_BUFFER_WIDTH;
+    
+    return (cColor);
+}
