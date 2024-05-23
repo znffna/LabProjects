7ef05e5b7cd309a7847a5167077f6bf72e8f3583
znffna <znffna@gmail.com>
2024-05-23 21:13:18 +0900

    따라하기(10).pdf - 주석위치 변경

--- LabProject09/GameFramework.h
+++ LabProject09/GameFramework.h
@@ -16,47 +16,49 @@ private:
 	int m_nWndClientWidth;
 	int m_nWndClientHeight;
 
+	// DXGI 팩토리 인터페이스에 대한 포인터이다.
 	IDXGIFactory4 *m_pdxgiFactory;
-// DXGI 팩토리 인터페이스에 대한 포인터이다.
-	IDXGISwapChain3 *m_pdxgiSwapChain;
-// 스왑 체인 인터페이스에 대한 포인터이다. 주로 디스플레이를 제어하기 위하여 필요하다.
-	ID3D12Device *m_pd3dDevice;
-// Direct3D 디바이스 인터페이스에 대한 포인터이다. 주로 리소스를 생성하기 위하여 필요하다.
 
+// 스왑 체인 인터페이스에 대한 포인터이다. 주로 디스플레이를 제어하기 위하여 필요하다.
+	IDXGISwapChain3 *m_pdxgiSwapChain;
+
+// Direct3D 디바이스 인터페이스에 대한 포인터이다. 주로 리소스를 생성하기 위하여 필요하다.
+	ID3D12Device *m_pd3dDevice;
+
+	// MSAA 다중 샘플링을 활성화하고 다중 샘플링 레벨을 설정한다.
 	bool m_bMsaa4xEnable = false;
 	UINT m_nMsaa4xQualityLevels = 0;
-// MSAA 다중 샘플링을 활성화하고 다중 샘플링 레벨을 설정한다.
 
+	// 스왑 체인의 후면 버퍼 인덱스이다.
 	static const UINT m_nSwapChainBuffers = 2;
-// 스왑 체인의 후면 버퍼 인덱스이다.
+	// 현재 스왑 체인의 후면 버퍼 인덱스이다.
 	UINT m_nSwapChainBufferIndex;
-// 현재 스왑 체인의 후면 버퍼 인덱스이다.
 
+	// 렌더 타겟 버퍼. 서술자 힙 인터페이스 포인터, 렌더 타겟 서술자 원소의 크기이다.
 	ID3D12Resource* m_ppd3dRenderTargetBuffers[m_nSwapChainBuffers];
 	ID3D12DescriptorHeap* m_pd3dRtvDescriptorHeap;
 	UINT m_nRtvDescriptorIncrementSize;
-// 렌더 타겟 버퍼. 서술자 힙 인터페이스 포인터, 렌더 타겟 서술자 원소의 크기이다.
 
+	// 깊이-스텐실 버퍼. 서술자 힙 인터페이스 포인터, 깊이-스텐실 서술자 원소의 크기이다.
 	ID3D12Resource* m_pd3dDepthStencilBuffer;
 	ID3D12DescriptorHeap* m_pd3dDsvDescriptorHeap;
 	UINT m_nDsvDescriptorIncrementSize;
-// 깊이-스텐실 버퍼. 서술자 힙 인터페이스 포인터, 깊이-스텐실 서술자 원소의 크기이다.
 
+	// 명령 큐, 명령 할당자, 명령 리시트 인터페이스 포인터이다.
 	ID3D12CommandQueue* m_pd3dCommandQueue;
 	ID3D12CommandAllocator* m_pd3dCommandAllocator;
 	ID3D12GraphicsCommandList* m_pd3dCommandList;
-// 명령 큐, 명령 할당자, 명령 리시트 인터페이스 포인터이다.
 
+	// 그래픽스 파이프라인 상태 객체에 대한 인터페이스 포인터이다.
 	ID3D12PipelineState* m_pd3dPipelineState;
-// 그래픽스 파이프라인 상태 객체에 대한 인터페이스 포인터이다.
 
+	// 펜스 인터페이스 포인터, 펜스의 값, 이벤트 핸들이다.
 	ID3D12Fence* m_pd3dFence;
 	UINT64 m_nFenceValues[m_nSwapChainBuffers];
 	HANDLE m_hFenceEvent;
-// 펜스 인터페이스 포인터, 펜스의 값, 이벤트 핸들이다.
 
+	// 씬을 위한 멤버 변수
 	CScene* m_pScene;
-// 씬을 위한 멤버 변수
 
 public:
 	CGameFramework();
