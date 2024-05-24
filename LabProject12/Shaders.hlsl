eb773ef8eb9f3a2118f91aeae60eb04b84eeec3a
znffna <znffna@gmail.com>
2024-05-24 12:44:01 +0900

    따라하기(13).pdf - Shader.hlsl 수정 완료

--- LabProject12/Shaders.hlsl
+++ LabProject12/Shaders.hlsl
@@ -42,3 +42,33 @@ float4 PSDiffused(VS_OUTPUT input) : SV_TARGET
 {
     return(input.color);
 }
+
+//정점 데이터와 인스턴싱 데이터를 위한 구조체이다.
+struct VS_INSTANCING_INPUT
+{
+float3 position : POSITION;
+float4 color : COLOR;
+float4x4 mtxTransform : WORLDMATRIX;
+float4 instanceColor : INSTANCECOLOR;
+};
+
+struct VS_INSTANCING_OUTPUT
+{
+    float4 position : SV_POSITION;
+    float4 color : COLOR;
+};
+
+VS_INSTANCING_OUTPUT VSInstancing(VS_INSTANCING_INPUT input)
+{
+    VS_INSTANCING_OUTPUT output;
+    output.position = mul(mul(mul(float4(input.position, 1.0f), input.mtxTransform),
+gmtxView), gmtxProjection);
+    output.color = input.color + input.instanceColor;
+    
+    return (output);
+}
+
+float4 PSInstancing(VS_INSTANCING_OUTPUT input) : SV_TARGET
+{
+    return (input.color);
+}
