f51d6e9f274f5ee524336fa322fa6b62d7218e8a
znffna <znffna@gmail.com>
2024-05-24 12:42:04 +0900

    따라하기(13).pdf - CShader 수정 필요
    ## CGameObject의 Matrix는 private된 변수인점.

--- LabProject12/Shader.h
+++ LabProject12/Shader.h
@@ -3,6 +3,13 @@
 #include "Object.h"
 #include "Camera.h"
 
+//인스턴스 정보(게임 객체의 월드 변환 행렬과 객체의 색상)를 위한 구조체이다. 
+struct VS_VB_INSTANCE
+{
+	XMFLOAT4X4 m_xmf4x4Transform;
+	XMFLOAT4 m_xmcColor;
+};
+
 //게임 객체의 정보를 셰이더에게 넘겨주기 위한 구조체(상수 버퍼)이다. 
 struct CB_GAMEOBJECT_INFO
 {
@@ -84,4 +91,34 @@ public:
 protected:
 	CGameObject** m_ppObjects = NULL;
 	int m_nObjects = 0;
+};
+
+class CInstancingShader : public CObjectsShader
+{
+public:
+	CInstancingShader();
+	virtual ~CInstancingShader();
+
+	virtual D3D12_INPUT_LAYOUT_DESC CreateInputLayout();
+	virtual D3D12_SHADER_BYTECODE CreateVertexShader(ID3DBlob** ppd3dShaderBlob);
+	virtual D3D12_SHADER_BYTECODE CreatePixelShader(ID3DBlob** ppd3dShaderBlob);
+	
+	virtual void CreateShader(ID3D12Device* pd3dDevice, ID3D12RootSignature
+		* pd3dGraphicsRootSignature);
+	
+	virtual void CreateShaderVariables(ID3D12Device* pd3dDevice, ID3D12GraphicsCommandList* pd3dCommandList);
+	virtual void UpdateShaderVariables(ID3D12GraphicsCommandList* pd3dCommandList);
+	virtual void ReleaseShaderVariables();
+	
+	virtual void BuildObjects(ID3D12Device* pd3dDevice, ID3D12GraphicsCommandList
+		* pd3dCommandList);
+	virtual void ReleaseObjects();
+	
+	virtual void Render(ID3D12GraphicsCommandList* pd3dCommandList, CCamera* pCamera);
+protected:
+	//인스턴스 정점 버퍼와 정점 버퍼 뷰이다. 
+	ID3D12Resource *m_pd3dcbGameObjects = NULL;
+	VS_VB_INSTANCE* m_pcbMappedGameObjects = NULL;
+
+	D3D12_VERTEX_BUFFER_VIEW m_d3dInstancingBufferView;
 };
