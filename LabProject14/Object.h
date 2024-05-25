795f25dab17d95ca69c6c210b3042b11d8fecddf
znffna <znffna@gmail.com>
2024-05-25 14:04:57 +0900

    따라하기(15).pdf - Object.h/cpp 파일 수정

--- LabProject14/Object.h
+++ LabProject14/Object.h
@@ -8,7 +8,7 @@ class CShader;
 class CGameObject
 {
 public:
-	CGameObject();
+	CGameObject(int nMeshes = 1);
 	virtual ~CGameObject();
 private:
 	int m_nReferences = 0;
@@ -17,11 +17,14 @@ public:
 	void Release() { if (--m_nReferences <= 0) delete this; }
 protected:
 	XMFLOAT4X4 m_xmf4x4World;
-	CMesh* m_pMesh = NULL;
+	//게임 객체는 여러 개의 메쉬를 포함하는 경우 게임 객체가 가지는 메쉬들에 대한 포인터와 그 개수이다. 
+	CMesh **m_ppMeshes = NULL;
+	int m_nMeshes = 0;
+
 	CShader* m_pShader = NULL;
 public:
 	void ReleaseUploadBuffers();
-	virtual void SetMesh(CMesh* pMesh);
+	void SetMesh(int nIndex, CMesh *pMesh);
 	virtual void SetShader(CShader* pShader);
 	virtual void Animate(float fTimeElapsed);
 	virtual void OnPrepareRender();
@@ -59,7 +62,7 @@ public:
 class CRotatingObject : public CGameObject
 {
 public:
-	CRotatingObject();
+	CRotatingObject(int nMeshes = 1);
 	virtual ~CRotatingObject();
 private:
 	XMFLOAT3 m_xmf3RotationAxis;
@@ -70,4 +73,40 @@ public:
 		m_xmf3RotationAxis = xmf3RotationAxis;
 	}
 	virtual void Animate(float fTimeElapsed);
+};
+
+class CHeightMapTerrain : public CGameObject
+{
+public:
+	CHeightMapTerrain(ID3D12Device* pd3dDevice, ID3D12GraphicsCommandList
+		* pd3dCommandList, ID3D12RootSignature* pd3dGraphicsRootSignature, LPCTSTR pFileName, int
+		nWidth, int nLength, int nBlockWidth, int nBlockLength, XMFLOAT3 xmf3Scale, XMFLOAT4
+		xmf4Color);
+	virtual ~CHeightMapTerrain();
+
+private:
+	//지형의 높이 맵으로 사용할 이미지이다. 
+	CHeightMapImage *m_pHeightMapImage;
+
+	//높이 맵의 가로와 세로 크기이다. 
+	int m_nWidth;
+	int m_nLength;
+
+	//지형을 실제로 몇 배 확대할 것인가를 나타내는 스케일 벡터이다. 
+	XMFLOAT3 m_xmf3Scale;
+public:
+	//지형의 높이를 계산하는 함수이다(월드 좌표계). 높이 맵의 높이에 스케일의 y를 곱한 값이다. 
+	float GetHeight(float x, float z) { return(m_pHeightMapImage->GetHeight(x /	m_xmf3Scale.x, z / m_xmf3Scale.z)* m_xmf3Scale.y);}
+	//지형의 법선 벡터를 계산하는 함수이다(월드 좌표계). 높이 맵의 법선 벡터를 사용한다. 
+	XMFLOAT3 GetNormal(float x, float z) {
+	return(m_pHeightMapImage->GetHeightMapNormal(int(x / m_xmf3Scale.x), 
+		int(z /	m_xmf3Scale.z))); }
+
+	int GetHeightMapWidth() { return(m_pHeightMapImage->GetHeightMapWidth()); }
+	int GetHeightMapLength() { return(m_pHeightMapImage->GetHeightMapLength()); }
+
+	XMFLOAT3 GetScale() { return(m_xmf3Scale); }
+	//지형의 크기(가로/세로)를 반환한다. 높이 맵의 크기에 스케일을 곱한 값이다. 
+	float GetWidth() { return(m_nWidth * m_xmf3Scale.x); }
+	float GetLength() { return(m_nLength * m_xmf3Scale.z); }
 };
