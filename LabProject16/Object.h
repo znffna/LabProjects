e219c798ec8d195859c7a44e75706cd97d503d87
znffna <znffna@gmail.com>
2024-05-25 15:11:15 +0900

    따라하기(17).pdf - Object.h/cpp 파일 수정

--- LabProject16/Object.h
+++ LabProject16/Object.h
@@ -56,6 +56,15 @@ public:
 public:
 	//게임 객체가 카메라에 보인는 가를 검사한다. 
 	bool IsVisible(CCamera *pCamera=NULL);
+
+public:
+	//모델 좌표계의 픽킹 광선을 생성한다. 
+	void GenerateRayForPicking(XMFLOAT3& xmf3PickPosition, XMFLOAT4X4& xmf4x4View,
+	XMFLOAT3* pxmf3PickRayOrigin, XMFLOAT3* pxmf3PickRayDirection);
+
+	//카메라 좌표계의 한 점에 대한 모델 좌표계의 픽킹 광선을 생성하고 객체와의 교차를 검사한다. 
+	int PickObjectByRayIntersection(XMFLOAT3& xmf3PickPosition, XMFLOAT4X4& xmf4x4View,
+	float* pfHitDistance);
 };
 
 class CRotatingObject : public CGameObject
